6956150cc4f72695c75ab5cb6b6e446fa6fa21ae
Albert <bomberocanelles@gmail.com>
2021-03-29 21:30:12 +0200

    Clase tarea con sus getters setters y constructor

--- src/clases/Tarea.java
+++ src/clases/Tarea.java
@@ -15,5 +15,96 @@ public class Tarea {
     private String resultado;
     private ArrayList<Etiqueta> etiquetas;
 
+    public Tarea(String nombre, String descripcion, ArrayList<Persona> listaParticipantes, String responsable, int prioridad, String fechaCreacion, String fechaFinalizacion, boolean estadoTarea, String resultado, ArrayList<Etiqueta> etiquetas) {
+        this.nombre = nombre;
+        this.descripcion = descripcion;
+        this.listaParticipantes = listaParticipantes;
+        this.responsable = responsable;
+        this.prioridad = prioridad;
+        this.fechaCreacion = fechaCreacion;
+        this.fechaFinalizacion = fechaFinalizacion;
+        this.estadoTarea = estadoTarea;
+        this.resultado = resultado;
+        this.etiquetas = etiquetas;
+    }
 
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public ArrayList<Persona> getListaParticipantes() {
+        return listaParticipantes;
+    }
+
+    public void setListaParticipantes(ArrayList<Persona> listaParticipantes) {
+        this.listaParticipantes = listaParticipantes;
+    }
+
+    public String getResponsable() {
+        return responsable;
+    }
+
+    public void setResponsable(String responsable) {
+        this.responsable = responsable;
+    }
+
+    public int getPrioridad() {
+        return prioridad;
+    }
+
+    public void setPrioridad(int prioridad) {
+        this.prioridad = prioridad;
+    }
+
+    public String getFechaCreacion() {
+        return fechaCreacion;
+    }
+
+    public void setFechaCreacion(String fechaCreacion) {
+        this.fechaCreacion = fechaCreacion;
+    }
+
+    public String getFechaFinalizacion() {
+        return fechaFinalizacion;
+    }
+
+    public void setFechaFinalizacion(String fechaFinalizacion) {
+        this.fechaFinalizacion = fechaFinalizacion;
+    }
+
+    public boolean isEstadoTarea() {
+        return estadoTarea;
+    }
+
+    public void setEstadoTarea(boolean estadoTarea) {
+        this.estadoTarea = estadoTarea;
+    }
+
+    public String getResultado() {
+        return resultado;
+    }
+
+    public void setResultado(String resultado) {
+        this.resultado = resultado;
+    }
+
+    public ArrayList<Etiqueta> getEtiquetas() {
+        return etiquetas;
+    }
+
+    public void setEtiquetas(ArrayList<Etiqueta> etiquetas) {
+        this.etiquetas = etiquetas;
+    }
 }
